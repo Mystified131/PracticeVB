14bd6431f0eeafdb498d3e65c9918d9edf02beff
Mystified131 <mystifiedthomas@gmail.com>
2019-05-08 09:02:17 -0500

    Adding tweaks

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -4,6 +4,9 @@
     Dim hgtInc As Double
     Dim xCoo As Int32
 
+    Dim ctr As Int32
+
+
     Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
 
         hgtInc = (Me.Height) / pixInc
@@ -28,5 +31,14 @@
         BackColor = Color.Wheat
         Debug.WriteLine("")
         Debug.WriteLine("Morning is here.")
+        ctr += 1
+
+        If (ctr >= 5) Then
+            Debug.WriteLine("")
+            Debug.WriteLine("Soon it will be noon.")
+            ctr = 0
+
+        End If
+
     End Sub
 End Class
